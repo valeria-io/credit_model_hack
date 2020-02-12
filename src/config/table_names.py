ccccad4070392c842df68ff6183e679b6032f848
valeria-io <cortezvacadiez@gmail.com>
2020-02-12 13:00:26 +0000

    Updated table names

--- src/config/table_names.py
+++ src/config/table_names.py
@@ -1,2 +1,2 @@
-BONDORA_RAW_TABLE_NAME = "`loans-experiment.bondora.loanbook`"
-BONDORA_EE_FILTERED_TABLE_NAME = "`loans-experiment.bondora.filtered_ee_loanbook`"
+BONDORA_RAW_TABLE_NAME = "`lbg-reboot-feb2020-team-18.loans.loanbook`"
+BONDORA_EE_FILTERED_TABLE_NAME = "`lbg-reboot-feb2020-team-18.loans.filtered_ee_loanbook`"
