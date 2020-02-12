01e2bc01c927142a383b3be27fa1d2cfb5b478a0
unknown <mconwa02@dcs.bbk.ac.uk>
2020-02-12 13:40:16 +0000

    adding logistic and random forest

--- src/machine_learning/logistic_regression.py
+++ src/machine_learning/logistic_regression.py
@@ -1,2 +1,11 @@
 import pandas as pd
+from src.features.pre_processing import load_ee_data_from_db
+from src.features.pre_processing import pre_process
+from src.features.feature_engineering import apply_feature_engineering
+from src.features.post_processing import post_process
 
+if __name__ == "main":
+    loan_df = load_ee_data_from_db()
+    loan_df = pre_process(loan_df)
+    loan_df = apply_feature_engineering(loan_df)
+    loan_df = post_process(loan_df)
---
+++ src/machine_learning/random_forest.py
@@ -0,0 +1,17 @@
+
+from src.features.pre_processing import load_ee_data_from_db
+from src.features.pre_processing import pre_process
+from src.features.feature_engineering import apply_feature_engineering
+from src.features.post_processing import post_process
+
+
+def random_forest_tuning(x_train, y_train):
+    return
+
+
+
+if __name__ == "main":
+    loan_df = load_ee_data_from_db()
+    loan_df = pre_process(loan_df)
+    loan_df = apply_feature_engineering(loan_df)
+    loan_df = post_process(loan_df)
